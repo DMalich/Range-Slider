e5b12bc76d4888adef94a4eb16f2f67f6bd040a7
DMalich <d.malich24@gmail.com>
2022-03-28 19:46:24 +0300

    Range slider

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const priceRange = document.getElementById('priceRange');
+const dollarsText = document.querySelector('.dollars')
+priceRange.addEventListener('input', (e) => updatePrice(e.target.value))
+
+const updatePrice = (priceInCents) => {
+    const dollars = Math.floor(priceInCents / 100).toString().padStart(2, '0');
+    const cents = (priceInCents % 100).toString().padStart(2, '0')
+    const price = `${dollars}.${cents}`;
+    dollarsText.innerText = price;
+}
+
+
